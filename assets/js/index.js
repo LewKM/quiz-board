f270d504e3591147b263091a3bdb0e3724a8e3ad
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-05-14 20:38:19 +0300

    Start doing final styling.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,18 +2,18 @@ function studentScore() {
     var total = 3;
     var score = 0;
 
-    var q1 = document.forms['quizForm']['q1'].value;
-    var q2 = document.forms['quizForm']['q2'].value;
-    var q3 = document.forms['quizForm']['q3'].value;
+    var q1 = document.forms['javascriptQuiz']['q1'].value;
+    var q2 = document.forms['javascriptQuiz']['q2'].value;
+    var q3 = document.forms['javascriptQuiz']['q3'].value;
 
     for (var i = 1; i <= total; i++) {
         if (eval('q' + i) === null || eval('q' + i) == '') {
-            alert('You did not answer' + i);
+            alert('You did not answer question' + " " + i);
             return false;
         }
     }
 
-    var answers = ["a", "b", "a"]
+    var answers = ["a", "b", "a"];
 
     for (var i = 1; i <= total; i++) {
         if (eval('q' + i) == answers[i - 1]) {
